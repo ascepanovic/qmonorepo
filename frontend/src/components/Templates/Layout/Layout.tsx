e1fcef1ed620f6c007aa91d111dc3196f83bc814
Radule-Bulatovic <bulatoviclale00@gmail.com>
2023-07-04 14:17:29 +0200

    add: routes constant

--- frontend/src/components/Templates/Layout/Layout.tsx
+++ frontend/src/components/Templates/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Link, Outlet } from "react-router-dom";
 
 import { UserIcon } from "@/components/UI/UserIcon";
+import { ROUTES } from "@/constants";
 
 export const Layout: React.FC = () => {
   return (
@@ -8,7 +9,7 @@ export const Layout: React.FC = () => {
       <div className="flex min-h-screen flex-col items-center bg-main-bg">
         <div className="flex w-full items-center justify-between px-8 py-8 md:px-16">
           <Link
-            to={"/"}
+            to={ROUTES.INDEX}
             className="text-2xl tracking-[1rem] text-main md:text-3xl md:tracking-[4rem]"
           >
             QUIZ
--- frontend/src/routes/protected.tsx
+++ frontend/src/routes/protected.tsx
@@ -1,10 +1,11 @@
 import { Navigate } from "react-router-dom";
 
 import { Home, Profile, Room } from "@/components/Pages";
+import { ROUTES } from "@/constants";
 
 export const protectedRoutes = [
-  { path: "/", element: <Home /> },
-  { path: "/profile", element: <Profile /> },
-  { path: "/room", element: <Room /> },
-  { path: "*", element: <Navigate to="." /> },
+  { path: ROUTES.INDEX, element: <Home /> },
+  { path: ROUTES.PROFILE, element: <Profile /> },
+  { path: ROUTES.ROOM, element: <Room /> },
+  { path: "*", element: <Navigate to={ROUTES.INDEX} /> },
 ];
